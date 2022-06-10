3908862c9bc4890f3dfc0e65ff53ec0593872062
LAPTOP-G41OVT31\Rohit Reddy <reddyrohith390@gmail.com>
2022-06-11 01:43:04 +0530

    add - 986. Interval List Intersections

--- src/Day_4/Lec986_Interval_List_Intersections.java
+++ src/Day_4/Lec986_Interval_List_Intersections.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class Lec986 {
+public class Lec986_Interval_List_Intersections {
     public static void main(String[] args) {
 //        int[][] firstList = new int[][] {
 //                {0,2},
@@ -35,6 +35,7 @@ public class Lec986 {
         int minLength = Math.min(firstList_length, secondList_length);
         List<int[]> result = new ArrayList<>();
 
+        // if any of the array has no element then there is no intersections possible
         if (minLength == 0 ) {
             return new int[][]{};
         }
@@ -42,6 +43,7 @@ public class Lec986 {
         int first_list_index = 0;
         int second_list_index = 0;
 
+        // repeat until we exhaust both of the lists
         while (first_list_index < firstList_length && second_list_index < secondList_length) {
 
             int[] a = firstList[first_list_index];
@@ -52,21 +54,67 @@ public class Lec986 {
             int b1 = b[0];
             int b2 = b[1];
 
+            /* Case - 1
+
+             As B has exhausted its range increment B
+              (A1)_________________(A2)
+                   (B1)_____(B2)
+
+            ( inc A or B )
+              (A1)_________________(A2)
+              (B1)__________________(B2)
+             */
             if (b1 >= a1 && b2 <= a2) {
                 result.add(new int[]{b1, b2});
                 second_list_index++;
-            } else if (b1 > a1 && b1 <= a2) {
+            }
+            /* Case - 2
+                 As A has exhausted its range increment A
+                (A1)_________________(A2)
+                     (B1)_____________________(B2)
+
+                (A1)_________________(A2)
+                                     (B1)_____________________(B2)
+             */
+            else if (b1 > a1 && b1 <= a2) {
                 result.add(new int[]{b1, a2});
                 first_list_index++;
-            } else if(b1 < a1 && b2 < a2 && a1<=b2) {
+            }
+             /* Case - 3
+                 As B has exhausted its range increment B
+                        (A1)________________(A2)
+               (B1)_______________(B2)
+
+               Inc Either A Or B
+                                        (A1)_________________(A2)
+               (B1)_____________________(B2)
+             */
+            else if(b1 < a1 && b2 < a2 && a1<=b2) {
                 result.add(new int[]{a1, b2});
                 second_list_index++;
-            } else if (a1>=b1 && a2<=b2) {
+            }
+            /*
+                As A has exhausted its range increment A
+                        (A1)_________________(A2)
+                (B1)________________________________(B2)
+             */
+            else if (a1>b1 && a2<b2) {
                 result.add(new int[]{a1, a2});
                 first_list_index++;
-            } else if (b1>a2) {
+            }
+             /*
+                No Intersection ( inc A as it has lower bounds )
+                        (A1)______(A2)
+                                         (B1)__________(B2)
+             */
+            else if (b1>a2) {
                 first_list_index ++;
             }
+            /*
+                No Intersection ( inc B as it has lower bounds )
+                                       (A1)______(A2)
+                   (B1)__________(B2)
+             */
             else if (b2 < a1) {
                 second_list_index ++;
             }
